9d6cc24d9bc334535edfd98eec37c3d4696c901b
Mack <mackshah1994@gmail.com>
2019-06-10 13:07:48 +0530

    video changes

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { createBottomTabNavigator, createAppContainer, SafeAreaView } from 'react-navigation';
-import { Provider } from 'react-redux'
-import { store } from './app/store/store'
-
+import {StatusBar, StyleSheet, TouchableOpacity,Text} from 'react-native';
+import {createAppContainer, SafeAreaView} from 'react-navigation';
+import {Provider} from 'react-redux'
+import {store} from './app/store/store'
+import Video from 'react-native-video';
 import RootStack from './app/screens/tabs/tabs'
+import MainVideo from './app/images/vid.mp4'
 
 let Navigation = createAppContainer(RootStack);
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <SafeAreaView style={{flex : 1}}>
-          <Navigation />
-        </SafeAreaView>
-      </Provider>
-    );
-  }
+
+    componentDidMount() {
+        StatusBar.setHidden(true);
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isClosed: 0
+        }
+    }
+
+    onPress = () => {
+        this.setState({
+            isClosed: 1
+        })
+    };
+
+    render() {
+        if (this.state.isClosed === 0) {
+            return (
+                <Provider store={store}>
+                    <SafeAreaView style={{flex: 1}}>
+                        <Video source={MainVideo}
+                               ref={(ref) => {
+                                   this.player = ref
+                               }}
+                               style={styles.backgroundVideo} />
+
+                        <TouchableOpacity onPress={this.onPress} style={styles.submitButton}>
+                            <Text style={{color: 'white', padding: 10, margin: 10, textAlign: 'center'}}> Close </Text>
+                        </TouchableOpacity>
+                    </SafeAreaView>
+                </Provider>
+            );
+        } else {
+            return (
+                <Provider store={store}>
+                    <SafeAreaView style={{flex: 1}}>
+                        <Navigation/>
+                    </SafeAreaView>
+                </Provider>
+            );
+        }
+    }
 }
+
+const styles = StyleSheet.create({
+    backgroundVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        backgroundColor: 'black'
+    },
+    submitButton: {
+        flex:1,
+        position: 'absolute',
+        bottom:0,
+        right: 0,
+        justifyContent: 'flex-end',
+        alignSelf: 'flex-end',
+        backgroundColor: 'rgba(255,255,255,0.31)',
+        textAlign: 'center'
+
+    }
+});
--- app/screens/document/index.js
+++ app/screens/document/index.js
@@ -1,80 +1,87 @@
 import React from 'react';
-import { Text, View, ScrollView, Dimensions, Image, TouchableOpacity } from 'react-native';
+import {Text, View, ScrollView, Dimensions, Image, TouchableOpacity} from 'react-native';
 import Header from '../../commonComponent/headerComponent';
 import Card from '../../commonComponent/cardComponent'
-import { getDocument } from '../../actions/formAction'
-import { connect } from 'react-redux';
-import { BASE_URL } from '../../api/urlMapper'
+import {getDocument} from '../../actions/formAction'
+import {connect} from 'react-redux';
+import {BASE_URL} from '../../api/urlMapper'
 import ImageZoom from 'react-native-image-pan-zoom';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const { height, width } = Dimensions.get('window');
+const {height, width} = Dimensions.get('window');
 
 class DocumentScreen extends React.Component {
-  componentWillMount() {
-    this.state = {
-      isPopUpOpen: false,
-      currentImage: ''
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isPopUpOpen: false,
+            currentImage: ''
+        }
     }
-    this.props.getDocument()
-  }
-  static navigationOptions = {
-    title: 'ગેલેરી',
-  };
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <Header title="ગેલેરી" />
-        <ScrollView style={{ flex: 1 }}>
-          <View style={{ paddingTop: 10, flexDirection: 'row', flexWrap: 'wrap' }}>
-            {this.props.document && this.props.document.map((doc, i) => {
-              return <TouchableOpacity onPress={() => {
-                this.setState({
-                  isPopUpOpen: true,
-                  currentImage: `${doc.event_file}`
-                })
-              }}><Image key={i}
-                resizeMode={'contain'}
-                style={{ width: width * 0.45, height: width * 0.30, margin: 5 }}
-                source={{ uri: `${doc.event_file}` }} /></TouchableOpacity>
-            })}
-          </View>
-        </ScrollView>
-        {this.state.isPopUpOpen && <View style={{
-          backgroundColor: "rgba(255,255,255,0.8)",
-          right: 0, left: 0, top: 50, bottom: 0,
-          position: 'absolute'
-        }}>
-          <TouchableOpacity style={{ alignItems: 'flex-end' , padding:5}} onPress={() => {
-            this.setState({
-              isPopUpOpen: false,
-              currentImage: ''
-            })
-          }}>
-            <Ionicons name='ios-close-circle' size={20} color={'gray'} />
-          </TouchableOpacity>
-          <ImageZoom cropWidth={Dimensions.get('window').width}
-            cropHeight={Dimensions.get('window').height}
-            imageWidth={width * 0.70}
-            imageHeight={height * 0.70}>
-            <Image resizeMode={'contain'}
-              style={{ width: width * 0.70, height: height * 0.70, margin: 5 }}
-              source={{ uri: this.state.currentImage }} />
-          </ImageZoom>
-        </View>}
-      </View>
-    );
-  }
+
+    componentWillMount() {
+        this.props.getDocument()
+    }
+
+    static navigationOptions = {
+        title: 'ગેલેરી',
+    };
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <Header title="ગેલેરી"/>
+                <ScrollView style={{flex: 1}}>
+                    <View style={{paddingTop: 10, flexDirection: 'row', flexWrap: 'wrap'}}>
+                        {this.props.document && this.props.document.map((doc, i) => {
+                            return <TouchableOpacity key={i} onPress={() => {
+                                this.setState({
+                                    isPopUpOpen: true,
+                                    currentImage: `${doc.event_file}`
+                                })
+                            }}><Image
+                                resizeMode={'contain'}
+                                style={{width: width * 0.45, height: width * 0.30, margin: 5}}
+                                source={{uri: `${doc.event_file}`}}/></TouchableOpacity>
+                        })}
+                    </View>
+                </ScrollView>
+                {this.state.isPopUpOpen && <View style={{
+                    backgroundColor: "rgba(255,255,255,0.8)",
+                    right: 0, left: 0, top: 10, bottom: 0,
+                    position: 'absolute'
+                }}>
+                    <TouchableOpacity style={{alignItems: 'flex-end', padding: 5}} onPress={() => {
+                        this.setState({
+                            isPopUpOpen: false,
+                            currentImage: ''
+                        })
+                    }}>
+                        <Ionicons name='ios-close-circle' size={20} color={'gray'}/>
+                    </TouchableOpacity>
+                    <ImageZoom cropWidth={Dimensions.get('window').width}
+                               cropHeight={Dimensions.get('window').height}
+                               imageWidth={width * 0.70}
+                               imageHeight={height * 0.70}>
+                        <Image resizeMode={'contain'}
+                               style={{width: width * 0.70, height: height * 0.70, margin: 5}}
+                               source={{uri: this.state.currentImage}}/>
+                    </ImageZoom>
+                </View>}
+            </View>
+        );
+    }
 }
 
 const mapStateToProps = state => {
-  return {
-    document: state.root.form.document
-  }
+    return {
+        document: state.root.form.document
+    }
 };
 
 const mapDispatchToProps = {
-  getDocument
+    getDocument
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(DocumentScreen)
